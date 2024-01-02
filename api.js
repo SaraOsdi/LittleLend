268946bcbe68da05fd97785d0503ef1092a710e0
DanielBelz1997 <128147522+DanielBelz1997@users.noreply.github.com>
2024-01-02 21:16:17 +0200

    reformating again

--- api.js
+++ api.js
@@ -1,21 +1,14 @@
 import { Router } from "express";
 const router = Router();
-import { login } from "./controllers/auth.js";
+import { getAllBorrowers, adminLogin } from "./controllers/auth.js";
 
-router.get("/login", login);
+router.get("/getAllBorrowers", getAllBorrowers);
 
 // take all those routes and out them like i did in the login route. i know
 // it needs to be post so you can check if a user is in the system or not,
 // but i will handle this tommorow evening. more details in the email
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-router.get("/", (req, res) => {
-  res.send("Hello, this is the home page!");
-});
+router.post("/adminLogin", adminLogin);
 
 router.get("/api/items", (req, res) => {
   // Check if the request is coming from an authenticated admin
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,27 @@
-import logger from "../modules/logger.js";
 import pb from "../db/pocketbase.js";
+import { errorHandler } from "../error.js";
 
-export async function login(req, res) {
+export async function getAllBorrowers(req, res) {
   try {
     const records = await pb.collection("borrowers").getFullList({});
     console.log(records);
     res.json({ records });
   } catch (error) {
     console.log(error);
-    logger.error("follow the error message: ", error);
+    await errorHandler(res, error, "falied to fetch the borrowers list");
+  }
+}
+
+export async function adminLogin(req, res) {
+  try {
+    const authData = await pb.admins.authWithPassword(
+      "belzdaniel6@gmail.com",
+      "1234567891"
+    );
+    console.log(authData);
+    res.json({ token: authData.token });
+  } catch (error) {
+    console.log(error);
+    await errorHandler(res, error, "Failed to authenticate. try again");
   }
 }
