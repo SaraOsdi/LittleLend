dfc232f1d2a179fb2783886d8263634e70cf4e18
DanielBelz1997 <128147522+DanielBelz1997@users.noreply.github.com>
2024-01-05 10:05:48 +0200

    vds

--- error.js
+++
@@ -1,6 +0,0 @@
-import logger from "./modules/logger.js";
-
-export async function errorHandler(res, error, details) {
-  logger.error(details, { error });
-  res.status(400).json({ message: details, error });
-}
